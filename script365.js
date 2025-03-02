1121bac52a11a6855abf335856a91150d84fe59f
PICNICK <118606314+Picnick0176@users.noreply.github.com>
2025-03-02 13:53:02 +0700

    Update script365.js

--- script365.js
+++ script365.js
@@ -3,7 +3,7 @@ document.getElementById("startBtn").addEventListener("click", function() {
 
     // ตรวจสอบรหัสผ่าน
     if (password === "03038") {  // เปลี่ยน "yourPassword" เป็นรหัสที่ถูกต้อง
-        window.location.href = "Homepage.html"; // เปลี่ยนไปยังหน้า HTML ใหม่
+        window.location.href = "./Homepage.html"; // เปลี่ยนไปยังหน้า HTML ใหม่
     } else {
         alert("รหัสไม่ถูกต้อง!"); // แสดงข้อความเมื่อรหัสไม่ถูกต้อง
     }
